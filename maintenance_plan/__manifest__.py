aacbf4005a6ee7fc82a0568c8127a24f455ca039
Nadege <nadege.grandjean@logicasoft.eu>
2023-08-29 16:38:34 +0200

    [28446][FIX] Fix anomalies with maintenance plan Maintenance plan base date configuration on 'done date' is not compatible with horizon configuration. Reset the standard behavior if there's a planning horizon on a maintenance plan based on date not set at request creation (here 'done date', but could be another date added in customer repo or on future evol), and add warning on configuration form

--- maintenance_plan/__manifest__.py
+++ maintenance_plan/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Maintenance Plan",
     "summary": "Extends preventive maintenance planning",
-    "version": "15.0.1.9",
+    "version": "15.0.1.10",
     "author": "Camptocamp SA, ForgeFlow, Odoo Community Association (OCA) (Forked by Logicasoft)",
     "license": "AGPL-3",
     "category": "Maintenance",
